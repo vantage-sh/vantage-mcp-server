7f85552d9b43b6399faf7b0b9b2fa91023dfcd5b
Nelson Crespo <nelson@vantage.sh>
2025-04-03 16:32:47 -0400

    feat: Add tool to fetch cost reports.

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
 	"github.com/metoro-io/mcp-golang"
 	"github.com/metoro-io/mcp-golang/transport/stdio"
+	"github.com/vantage-sh/vantage-go/vantagev2/vantage/costs"
+	"os"
 )
 
 func main() {
+	bearerToken, found := os.LookupEnv("VANTAGE_BEARER_TOKEN")
+	if !found {
+		panic("VANTAGE_BEARER_TOKEN not found")
+	}
+
 	done := make(chan struct{})
 	server := mcp_golang.NewServer(stdio.NewStdioServerTransport())
 
@@ -23,6 +35,35 @@ func main() {
 
 			return mcp_golang.NewResourceResponse(resource), nil
 		})
+	if err != nil {
+		panic(err)
+	}
+
+	authInfo := runtime.ClientAuthInfoWriterFunc(func(req runtime.ClientRequest, reg strfmt.Registry) error {
+		err := req.SetHeaderParam("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+		return err
+	})
+	type ListCostReportsParams struct {
+	}
+	err = server.RegisterTool("list-cost-reports", "List all cost reports available", func(_params ListCostReportsParams) (*mcp_golang.ToolResponse, error) {
+		client := costs.NewClientWithBearerToken("api.vantage.sh", "/v2", "https", bearerToken)
+		params := costs.NewGetCostReportsParams()
+		var limit int32 = 10
+		params.SetLimit(&limit)
+
+		response, err := client.GetCostReports(params, authInfo)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("Error fetching cost reports: %v", err))
+		}
+
+		costReports, err := json.Marshal(response.Payload)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("Error marshalling cost reports: %v", err))
+		}
+
+		content := mcp_golang.NewTextContent(string(costReports))
+		return mcp_golang.NewToolResponse(content), nil
+	})
 	if err != nil {
 		panic(err)
 	}
