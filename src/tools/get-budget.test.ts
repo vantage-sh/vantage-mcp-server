1191e4c0c830799c33ca2e9f10661028122f83dd
perintyler <perintyler@gmail.com>
2026-04-01 16:05:31 -0400

    fix: Budget test.

--- src/tools/get-budget.test.ts
+++ src/tools/get-budget.test.ts
@@ -1,7 +1,18 @@
 import { type GetBudgetResponse, pathEncode } from "@vantage-sh/vantage-client";
 import { expect } from "vitest";
 import tool from "./get-budget";
-import { requestsInOrder, testTool } from "./utils/testing";
+import {
+	type ExecutionTestTableItem,
+	type ExtractOutputSchema,
+	type ExtractValidators,
+	type InferValidators,
+	requestsInOrder,
+	type SchemaTestTableItem,
+	testTool,
+} from "./utils/testing";
+
+type Validators = ExtractValidators<typeof tool>;
+type OutputSchema = ExtractOutputSchema<typeof tool>;
 
 const success: GetBudgetResponse = {
 	token: "bgt_123",
@@ -14,82 +25,89 @@ const success: GetBudgetResponse = {
 	cost_report_token: "crt_123",
 };
 
-testTool(
-	tool,
-	[
-		{
-			name: "takes budget_token",
-			data: {
-				budget_token: "bgt_123",
-			},
+const argumentSchemaTests: SchemaTestTableItem<Validators>[] = [
+	{
+		name: "takes budget_token",
+		data: {
+			budget_token: "bgt_123",
+			include_performance: undefined,
 		},
-		{
-			name: "with include_performance",
-			data: {
+	},
+	{
+		name: "with include_performance",
+		data: {
+			budget_token: "bgt_123",
+			include_performance: true,
+		},
+	},
+];
+
+const executionTests: ExecutionTestTableItem<Validators, OutputSchema>[] = [
+	{
+		name: "successful call without include_performance",
+		apiCallHandler: requestsInOrder([
+			{
+				endpoint: `/v2/budgets/${pathEncode("bgt_123")}`,
+				params: {},
+				method: "GET",
+				result: {
+					ok: true,
+					data: success,
+				},
+			},
+		]),
+		handler: async ({ callExpectingSuccess }) => {
+			const res = await callExpectingSuccess({
+				budget_token: "bgt_123",
+				include_performance: undefined,
+			});
+			expect(res).toEqual(success);
+		},
+	},
+	{
+		name: "successful call with include_performance",
+		apiCallHandler: requestsInOrder([
+			{
+				endpoint: `/v2/budgets/${pathEncode("bgt_123")}`,
+				params: { include_performance: true },
+				method: "GET",
+				result: {
+					ok: true,
+					data: success,
+				},
+			},
+		]),
+		handler: async ({ callExpectingSuccess }) => {
+			const res = await callExpectingSuccess({
 				budget_token: "bgt_123",
 				include_performance: true,
-			},
+			});
+			expect(res).toEqual(success);
 		},
-	],
-	[
-		{
-			name: "successful call without include_performance",
-			apiCallHandler: requestsInOrder([
-				{
-					endpoint: `/v2/budgets/${pathEncode("bgt_123")}`,
-					params: {},
-					method: "GET",
-					result: {
-						ok: true,
-						data: success,
-					},
-				},
-			]),
-			handler: async ({ callExpectingSuccess }) => {
-				const res = await callExpectingSuccess({ budget_token: "bgt_123" });
-				expect(res).toEqual(success);
-			},
-		},
-		{
-			name: "successful call with include_performance",
-			apiCallHandler: requestsInOrder([
-				{
-					endpoint: `/v2/budgets/${pathEncode("bgt_123")}`,
-					params: { include_performance: true },
-					method: "GET",
-					result: {
-						ok: true,
-						data: success,
-					},
-				},
-			]),
-			handler: async ({ callExpectingSuccess }) => {
-				const res = await callExpectingSuccess({
-					budget_token: "bgt_123",
-					include_performance: true,
-				});
-				expect(res).toEqual(success);
-			},
-		},
-		{
-			name: "unsuccessful call",
-			apiCallHandler: requestsInOrder([
-				{
-					endpoint: `/v2/budgets/${pathEncode("bgt_notfound")}`,
-					params: {},
-					method: "GET",
-					result: {
-						ok: false,
-						errors: [{ message: "Budget not found" }],
-					},
-				},
-			]),
-			handler: async ({ callExpectingMCPUserError }) => {
-				const err = await callExpectingMCPUserError({ budget_token: "bgt_notfound" });
-				expect(err.exception).toEqual({
+	},
+	{
+		name: "unsuccessful call",
+		apiCallHandler: requestsInOrder([
+			{
+				endpoint: `/v2/budgets/${pathEncode("bgt_notfound")}`,
+				params: {},
+				method: "GET",
+				result: {
+					ok: false,
 					errors: [{ message: "Budget not found" }],
-				});
+				},
 			},
+		]),
+		handler: async ({ callExpectingMCPUserError }) => {
+			const err = await callExpectingMCPUserError({
+				budget_token: "bgt_notfound",
+				include_performance: undefined,
+			});
+			expect(err.exception).toEqual({
+				errors: [{ message: "Budget not found" }],
+			});
 		},
-	]
-);
+	},
+];
+
+testTool(tool, argumentSchemaTests, executionTests);
