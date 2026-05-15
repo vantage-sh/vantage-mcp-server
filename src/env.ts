bc3fc8d334c61dbd35bd4c7e99709e9d7c86a294
jer-k <jeremyk112@gmail.com>
2026-05-14 17:51:06 -0700

    feat: Add optional otel tracing.

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,10 @@
 // Omit the ENVIRONMENT that is hardcoded into the wrangler files
 export type AppEnv = Omit<Env, "ENVIRONMENT"> & {
   ENVIRONMENT: "development" | "production";
+  OTLP_TRACES_ENDPOINT?: string;
+  OTLP_HEADERS?: string;
+  OTEL_RESOURCE_ATTRIBUTES?: string;
+  OTEL_SERVICE_NAME?: string;
+  OTEL_TRACES_SAMPLE_RATE?: string;
+  SENTRY_RELEASE?: string;
 };
