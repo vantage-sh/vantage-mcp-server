0c5ca46a31d98a77b2ffbd51783bc53e81c4d2ed
Samer Khatib <skhatib07@outlook.com>
2026-05-26 17:09:13 -0400

    fix: Move delete recs views to the correct folder.

--- src/tools/recommendation-views/index.ts
+++ src/tools/recommendation-views/index.ts
@@ -2,3 +2,4 @@ import "./list-recommendation-views";
 import "./get-recommendation-view";
 import "./create-recommendation-view";
 import "./update-recommendation-view";
+import "./delete-recommendation-view";
